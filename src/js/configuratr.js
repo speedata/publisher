22f1d83f783b698a717ddebace8115f855bfe363
Patrick Gundlach <gundlach@speedata.de>
2012-04-25 10:12:47 +0200

    sections [...] in config file

--- src/js/configuratr.js
+++ src/js/configuratr.js
@@ -5,26 +5,33 @@ var fs = require("fs"),
 function Configuratr() {
 	this.options = {}
 	this.read = function(filename) {
+		var current_section = "options"
 		var contents = fs.readFileSync(filename,'utf8').replace(/(\s*#.*|\s*)$/gm,""),
 		    lines = contents.split("\n"),
 			kv
 		for (var i = 0; i < lines.length; i++) {
+			var is_new_section = lines[i].match(/\[(.*)\]/)
+			if (is_new_section) {
+				current_section = is_new_section[1]
+				this[current_section] = {}
+			}
 			kv = lines[i].split(/\s*=\s*/)
 			if (kv.length == 2) {
-				if (this.options[kv[0]]) {
-					if (typeof(this.options[kv[0]]) == "string" || typeof(this.options[kv[0]]) == "number" ) {
-						this.options[kv[0]] = new Array(this.options[kv[0]],kv[1])
+				if (this[current_section][kv[0]]) {
+					if (typeof(this[current_section][kv[0]]) == "string" || typeof(this[current_section][kv[0]]) == "number" ) {
+						this[current_section][kv[0]] = new Array(this[current_section][kv[0]],kv[1])
 					} else {
-						this.options[kv[0]].push(kv[1])
+						this[current_section][kv[0]].push(kv[1])
 					}
 				} else {
-					this.options[kv[0]] = kv[1]
+					this[current_section][kv[0]] = kv[1]
 				}
 			}
 		};
 	}
-	this.getString = function (key) {
-		var tmp = this.options[key]
+	this.getString = function (key,section) {
+		var current_section = typeof(section) == "undefined" ?  "options" : section
+		var tmp = this[current_section][key]
 		if (typeof(tmp) == "string") {
 			return tmp
 		} else if (typeof(tmp) == "object") {
@@ -34,8 +41,9 @@ function Configuratr() {
 		}
 	}
 
-	this.getNumber = function (key) {
-		var tmp = this.options[key]
+	this.getNumber = function (key,section) {
+		var current_section = typeof(section) == "undefined" ?  "options" : section
+		var tmp = this[current_section][key]
 		if (typeof(tmp) == "string") {
 			return parseInt(tmp)
 		} else if (typeof(tmp) == "number") {
@@ -47,8 +55,9 @@ function Configuratr() {
 		}
 	}
 
-	this.getArray = function(key) {
-		var tmp = this.options[key]
+	this.getArray = function(key,section) {
+		var current_section = typeof(section) == "undefined" ?  "options" : section
+		var tmp = this[current_section][key]
 		if (util.isArray(tmp)) {
             return tmp
         } else {
