2849f8189ae788a022ed8ddef58806d4ba3301cf
Patrick Gundlach <gundlach@speedata.de>
2014-04-20 13:02:08 +0200

    Template for mathjax in sourcedoc

--- third/locco/template.lua
+++ third/locco/template.lua
@@ -1,13 +1,28 @@
 module(..., package.seeall)
 
-header = [[<!DOCTYPE html>
+header = [===[<!DOCTYPE html>
 
 <html>
 <head>
   <title>%title%</title>
   <meta http-equiv="content-type" content="text/html; charset=UTF-8">
   <link rel="stylesheet" media="all" href="locco.css" >
-  <script type="text/javascript" src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS_HTML"></script>
+  <script type="text/javascript" src="mj/MathJax.js?config=TeX-AMS_HTML"></script>
+  <script type="text/x-mathjax-config">
+    MathJax.Hub.Config({
+      extensions: ["tex2jax.js"],
+      jax: ["input/TeX","output/HTML-CSS"],
+      menuSettings: {zoom: "Double-Click", zscale: "300%"},
+      tex2jax: {inlineMath: [["\\(","\\)"]]},
+      MathMenu: {showRenderer: false},
+      "HTML-CSS": {
+          availableFonts: ["TeX"],
+          preferredFont: "TeX",
+          imageFont: null
+      }
+    });
+  </script>
+
 </head>
 <body>
   <div id="container">
@@ -24,7 +39,7 @@ header = [[<!DOCTYPE html>
         </tr>
       </thead>
       <tbody>
-]]
+]===]
 
 jump_start = [[
 <div id="jump_to">
