1077e3250ac13bb79d8f10d2d3deebd0e16300a2
Patrick Gundlach <gundlach@speedata.de>
2014-08-21 15:28:38 +0200

    Do not encourage break at /
    All special characters should go in the 'allowbreak'-list

--- src/lua/publisher.lua
+++ src/lua/publisher.lua
@@ -1632,11 +1632,6 @@ function mknodes(str,fontfamily,parameter)
                 local g = node.new(glue_node)
                 g.spec = node.new(glue_spec_node)
                 head,last = node.insert_after(head,last,g)
-            -- elseif char == ',' or char == '/' then
-            elseif char == '/' then
-                local pen = node.new("penalty")
-                pen.penalty = 0
-                head,last = node.insert_after(head,last,pen)
             elseif parameter.allowbreak and string.find(parameter.allowbreak, char,1,true) then
                 local pen = node.new("penalty")
                 pen.penalty = 0
