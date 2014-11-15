speedata/publisher

commit 71c9feb9ba1cd85f7a078d054157b4b0c11035bb

New dynamic table headers
Table rows (Tr) can be propagated as future table headers

src/lua/publisher/tabular.lua
@@ -37,7 +37,7 @@ end
 --- for a rule of thumb how objects are arranged in a table cell. I am not sure if we should fully follow
 --- the HTML way.
 ---
---- The returned array is a list of arrays. The inner arrays contain the objects to be stacked from left to right (“inline”)
+--- The inner arrays contain the objects to be stacked from left to right (“inline”)
 --- and the outer array is a list of block objects that are to be stacked from top to bottom:
 ---     { { img      },
 ---       { par      },
@@ -45,6 +45,7 @@ end
 ---       { table    }  }
 ---
 --- ![Objects in a table](img/objectsintable.svg)
+--- The table is stored in the objects
 function attach_objects_row( tab )
     -- For each block object (container) there is one row in block
     local td_elementname
@@ -55,8 +56,6 @@ function attach_objects_row( tab )
         if td_elementname == "Td" then
             local block = {}
             local inline = {}
-
-            local objects = {}
             for i,j in ipairs(td_contents) do
                 local eltname     = publisher.elementname(j,true)
                 local eltcontents = publisher.element_contents(j)
@@ -258,7 +257,7 @@ function calculate_columnwidth( self )
     local colmax,colmin = {},{}
 
     local current_row = 0
-    self.tablewidth_target = self.breite
+    self.tablewidth_target = self.width
     local columnwidths_given = false
 
     for _,tr in ipairs(self.tab) do
@@ -990,7 +989,6 @@ function typeset_row(self, tr_contents, current_row )
         --- ![Table cell vertical](img/tablecell3.svg)
         hlist = node.vpack(head,self.rowheights[current_row],"exactly")
 
-
         if publisher.options.trace then
             publisher.boxit(hlist)
         end
@@ -1026,6 +1024,11 @@ function typeset_row(self, tr_contents, current_row )
         err("(Internal error) Table is not complete.")
     end
     node.set_attribute(row,publisher.att_tr_shift_up,tr_contents.shiftup)
+    if tr_contents.sethead then
+        node.set_attribute(row,publisher.att_use_as_head,1)
+    end
+
+
     return row
 end
 
@@ -1092,36 +1095,21 @@ local function make_tablefoot(self,tr_contents,tablefoot_last,tablefoot,current_
     return current_row
 end
 --------------------------------------------------------------------------
+
+-- TODO: rename function: we don't calculate the height here
 local function calculate_height_and_connect_tablehead(self,tablehead_first,tablehead)
-    local ht_header, ht_first_header = 0, 0
     -- We connect all but the last row with the next row and remember the height in ht_header
     for z = 1,#tablehead_first - 1 do
-        ht_first_header = ht_first_header + tablehead_first[z].height  -- Tr oder Tablerule
         _,tmp = publisher.add_glue(tablehead_first[z],"tail",{ width = self.rowsep })
         tmp.next = tablehead_first[z+1]
         tablehead_first[z+1].prev = tmp
     end
 
     for z = 1,#tablehead - 1 do
-        ht_header = ht_header + tablehead[z].height  -- Tr or Tablerule
         _,tmp = publisher.add_glue(tablehead[z],"tail",{ width = self.rowsep })
         tmp.next = tablehead[z+1]
         tablehead[z+1].prev = tmp
     end
-
-    -- perhaps there is a last row, that is connected but its height is not
-    -- taken into account yet.
-    if #tablehead > 0 then
-        ht_header = ht_header + tablehead[#tablehead].height + self.rowsep  * ( #tablehead )
-    end
-
-    if #tablehead_first > 0 then
-        ht_first_header = ht_first_header + tablehead_first[#tablehead_first].height + self.rowsep * (#tablehead_first)
-    else
-        ht_first_header = ht_header
-    end
-
-    return ht_first_header,ht_header
 end
 
 local function calculate_height_and_connect_tablefoot(self,tablefoot,tablefoot_last)
@@ -1163,6 +1151,7 @@ function typeset_table(self)
     local tablehead = {}
     local tablefoot_last = {}
     local tablefoot = {}
+    local omit_head_on_pages = {}
     local rows = {}
     local break_above = true
     local filter = {}
@@ -1238,8 +1227,16 @@ function typeset_table(self)
         return publisher.empty_block()
     end
 
+    local tableheads_extra = {}
 
-    local ht_first_header, ht_header = calculate_height_and_connect_tablehead(self,tablehead_first,tablehead)
+
+    setmetatable(tableheads_extra, { __index = function(tbl,idx)
+        if idx < 1 then return nil end
+        return tbl[idx - 1]
+    end
+    })
+
+    calculate_height_and_connect_tablehead(self,tablehead_first,tablehead)
     local ht_footer,  ht_footer_last = calculate_height_and_connect_tablefoot(self,tablefoot,tablefoot_last)
 
     if not tablehead[1] then
@@ -1259,8 +1256,12 @@ function typeset_table(self)
     local ht_max     = self.optionen.ht_max
     -- The maximum heights are saved here for each table. Currently all tables must have the same height (see the metatable)
     local pagegoals = {}
+
     local function showheader( tablepart )
+        -- We can skip the header on pages where the first line is also
+        if omit_head_on_pages[tablepart] then return false end
         if tablepart_absolute == 1 and filter.tablehead_force_first then return true end
+        if tableheads_extra[tablepart_absolute] ~= nil then return true end
         if not filter.tablehead then return true end
         if math.fmod(tablepart_absolute,2) == math.fmod(startpage,2) then
             if filter.tablehead == "odd" then
@@ -1276,21 +1277,38 @@ function typeset_table(self)
             end
         end
     end
+    local function get_height_header(i)
+        if not showheader(i) then return 0 end
+        if tableheads_extra[i] then
+            return tableheads_extra[i].height + self.rowsep
+        end
+        if i == 1 then
+            return tablehead_first[1].height + self.rowsep
+        end
+        return tablehead[1].height + self.rowsep
+    end
+
     setmetatable(pagegoals, { __index = function(tbl,idx)
+                local ht_head = get_height_header(idx)
                 if idx == 1 then
-                    if showheader(idx) then
-                        return ht_current - ht_first_header - ht_footer
-                    else
-                        return ht_current - ht_footer
-                    end
+                    return ht_current - ht_head - ht_footer
+                elseif idx == -1 then
+                    return ht_max - ht_head - ht_footer_last
                 end
-                if showheader(idx) then
-                    return ht_max - ht_header - ht_footer
-                else
-                    return ht_max - ht_footer
-                end
+                return ht_max - ht_head - ht_footer
     end})
-    pagegoals[-1] = ht_max - ht_header - ht_footer_last
+
+
+    local function get_tablehead( page )
+        if s == 1 then
+            return tablehead_first[1]
+        end
+        if tableheads_extra[page] then
+            return node.copy_list(tableheads_extra[page])
+        end
+        return node.copy_list(tablehead[1])
+    end
+
 
     -- When we split the current table we return an array:
     local final_split_tables = {}
@@ -1324,10 +1342,18 @@ function typeset_table(self)
     local current_page = 1
 
     for i=1,#rows do
+        -- We can mark a row as "use_as_head" to turn the row into a dynamic head
+        local use_as_head = node.has_attribute(rows[i],publisher.att_use_as_head)
+        if use_as_head then
+            -- tablehead[1] = node.copy(rows[i])
+            tableheads_extra[#splits + 1] = node.copy(rows[i])
+        end
         local shiftup = node.has_attribute(rows[i],publisher.att_tr_shift_up) or 0
         if shiftup > 0 then
             rows[i].height = rows[i].height - shiftup
         end
+
+
         pagegoal = pagegoals[current_page]
         ht_row = rows[i].height + rows[i].depth
         break_above = node.has_attribute(rows[i],publisher.att_break_above) or -1
@@ -1343,6 +1369,12 @@ function typeset_table(self)
         extra_height = extra_height + ht_row
         local fits_in_table = accumulated_height + extra_height + space_above < pagegoal
         if not fits_in_table then
+            if node.has_attribute(rows[i],publisher.att_use_as_head) == 1 then
+                -- the next line would be used as a header, so let's skip the
+                -- header on this page
+                omit_head_on_pages[#splits + 1] = true
+            end
+
             if shiftup > 0 then
                 rows[i].height = rows[i].height + shiftup
             end
@@ -1393,16 +1425,11 @@ function typeset_table(self)
             end
         else
             if showheader(s-1) then
-                if s == 2 then
-                    -- first page
-                    thissplittable[#thissplittable + 1] = tablehead_first[1]
-                else
-                    -- page > 1
-                    thissplittable[#thissplittable + 1] = node.copy_list(tablehead[1])
-                end
+                thissplittable[#thissplittable + 1] = get_tablehead(s-1)
             end
         end
-        for i = first_row_in_new_table ,splits[s]  do
+
+        for i = first_row_in_new_table,splits[s]  do
             if i > first_row_in_new_table then
                 space_above = node.has_attribute(rows[i],publisher.att_space_amount) or 0
             else
@@ -1415,7 +1442,7 @@ function typeset_table(self)
 
         last_tr_data = node.has_attribute(thissplittable[#thissplittable - 1],publisher.att_tr_dynamic_data)
 
-        -- only refomat the foot when we have dynamic data _and_ have a foot to reformat.
+        -- only reformat the foot when we have dynamic data _and_ have a foot to reformat.
         if last_tr_data and self.tablefoot_contents then
             -- we have some data attached to table rows, so we re-format the footer
             local val = dynamic_data[last_tr_data]

doc/commands-xml/commands.xml
@@ -5675,6 +5675,14 @@
         <para>Auf diese Daten können im Tabellenkopf und Tabellenfuß mit <tt>$_last_tr_data</tt> zugegriffen werden.</para>
       </description>
     </attribute>
+    <attribute name="sethead" type="yesno" optional="yes">
+      <description xml:lang="en">
+        <para>Use this line for future table heads.</para>
+      </description>
+      <description xml:lang="de">
+        <para>Diese Zeile für zukünftige Kopfzeilen benutzen.</para>
+      </description>
+    </attribute>
     <remark xml:lang="de">
       <para>Das Attribut minhöhe war in einer vorigen Version minhoehe.</para>
     </remark>

schema/layoutschema-de.rng
@@ -3031,6 +3031,16 @@
                <a:documentation>Auf diese Daten können im Tabellenkopf und Tabellenfuß mit $_last_tr_data zugegriffen werden.</a:documentation>
             </attribute>
          </optional>
+         <optional>
+            <attribute name="setzekopf">
+               <a:documentation>Diese Zeile für zukünftige Kopfzeilen benutzen.</a:documentation>
+               <choice>
+                  <value>ja</value>
+                  <value>nein</value>
+                  <text/>
+               </choice>
+            </attribute>
+         </optional>
          <oneOrMore>
             <optional>
                <ref name="e_Message"/>

schema/layoutschema-en.rng
@@ -3017,6 +3017,16 @@
                <a:documentation>Data that can be accessed via $_last_tr_data in the tablefoot and tablehead.</a:documentation>
             </attribute>
          </optional>
+         <optional>
+            <attribute name="sethead">
+               <a:documentation>Use this line for future table heads.</a:documentation>
+               <choice>
+                  <value>yes</value>
+                  <value>no</value>
+                  <text/>
+               </choice>
+            </attribute>
+         </optional>
          <oneOrMore>
             <optional>
                <ref name="e_Message"/>

schema/translations.xml
@@ -284,6 +284,7 @@
     <attribute key="trace" en="trace" de="trace"/>
     <attribute key="trim" en="trim" de="beschnittzugabe"/>
     <attribute key="type" en="type" de="typ" />
+    <attribute key="sethead" en="sethead" de="setzekopf" />
     <attribute key="valign" en="valign" de="valign"/>
     <attribute key="value" en="value" de="wert" />
     <attribute key="variable" en="variable" de="variable"/>

src/lua/publisher.lua
@@ -65,6 +65,7 @@ att_space_amount   = 301
 att_break_below_forbidden = 400
 att_break_above           = 401
 att_omit_at_top           = 402
+att_use_as_head           = 403
 
 --- `att_is_table_row` is used in `tabular.lua` and if set to 1, it denotes
 --- a regular table row, and not a spacer. Spacers must not appear

src/lua/publisher/commands.lua
@@ -2215,7 +2215,7 @@ function commands.table( layoutxml,dataxml,optionen )
     tabular.optionen       = optionen or { ht_max=99999*2^16 } -- FIXME! Test - this is for tabular in tabular
     tabular.layoutxml      = layoutxml
     tabular.dataxml        = dataxml
-    tabular.breite         = width
+    tabular.width          = width
     tabular.fontfamily     = fontfamily
     tabular.padding_left   = padding
     tabular.padding_top    = padding
@@ -2293,6 +2293,7 @@ function commands.tr( layoutxml,dataxml )
         ["minheight"]       = "number",
         ["top-distance"]    = "rawstring",
         ["break-below"]     = "string",
+        ["sethead"]         = "boolean",
     }
 
     for attname,atttyp in pairs(attribute) do

src/lua/translations.lua
@@ -448,6 +448,7 @@ values = {
     ["trace"] = {de = "trace", en = "trace", },
     ["trim"] = {de = "beschnittzugabe", en = "trim", },
     ["type"] = {de = "typ", en = "type", },
+    ["sethead"] = {de = "setzekopf", en = "sethead", },
     ["valign"] = {de = "valign", en = "valign", },
     ["value"] = {de = "wert", en = "value", },
     ["variable"] = {de = "variable", en = "variable", },

commit c4eb8e9e310cec792742049f42dfee662a5f9d15

Bugfix: dynamic table header
Fix bug introduced in 71c9feb9. I should rename
variables and not just copy/paste 'em.

src/lua/publisher/tabular.lua
@@ -1167,6 +1167,7 @@ function typeset_table(self)
     local tablehead = {}
     local tablefoot_last = {}
     local tablefoot = {}
+    -- omit_head_on_pages is for dynamic headers (2)
     local omit_head_on_pages = {}
     local rows = {}
     local break_above = true
@@ -1338,7 +1339,7 @@ function typeset_table(self)
 
 
     local function get_tablehead_static( page )
-        if s == 1 then
+        if page == 1 then
             return tablehead_first[1]
         end
         return node.copy_list(tablehead[1])