50c544569cf3815ee20c078f8f3e191d9d5fa5aa
Patrick Gundlach <gundlach@speedata.de>
2017-07-31 11:51:46 +0200

    Fix PDF operators with underline
    This fixes #135

--- src/lua/publisher/fonts.lua
+++ src/lua/publisher/fonts.lua
@@ -342,7 +342,7 @@ function insert_underline( parent, head, start, typ)
     end
 
     local shift_down = ( dp - rule_width ) / 1.5
-    rule.data = string.format("q 0 g 0 G 0 %g w %s %g m %g %g l S Q", rule_width, dashpattern, -1 * shift_down, -wd, -1 * shift_down )
+    rule.data = string.format("q 0 g 0 G %g w %s  0 %g m %g %g l S Q", rule_width, dashpattern, -1 * shift_down, -wd, -1 * shift_down )
     rule.mode = 0
     parent.head = node.insert_before(parent.head,head,rule)
     return rule
