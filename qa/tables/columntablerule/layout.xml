speedata/publisher

commit 0cfc934f0689c8b4f76b0a3717dca5386fa4fc11

B: Tablerule too wide when only * widths given
This fixes #207

src/lua/publisher/tabular.lua
@@ -357,6 +357,13 @@ function calculate_columnwidth( self )
                 count_columns = i
             end
 
+            -- if stretch="no", we don't need to stretch/shrink anything
+            -- sum_real_widths == 0 happens when there are only * widths
+            -- given in the <Column width="..."/>  setting.
+            if self.autostretch ~= "max" and sum_real_widths ~= 0 then
+                self.tablewidth_target = sum_real_widths
+            end
+
             if columnwidths_given and count_stars == 0 then return end
 
             if count_stars > 0 then
@@ -364,8 +371,7 @@ function calculate_columnwidth( self )
 
                 -- now we know the number of *-columns and the sum of the fix colums, so that
                 -- we can distribute the remaining space
-                local to_distribute =  self.tablewidth_target - sum_real_widths - table.sum(self.column_distances,1,count_columns - 1)
-
+                local to_distribute = self.tablewidth_target - table.sum(self.column_distances,1,count_columns - 1)
                 i = 0
                 for _,column in ipairs(tr_contents) do
                     if publisher.elementname(column)=="Column" then

commit 9d231fbac6704cb947eddf43255dc5215448b520

B: Table rule too long
This fixes #207 and is related to 
0cfc934f0689c8b4f76b0a3717dca5386fa4fc11, which introduced new errors.

src/lua/publisher/tabular.lua
@@ -358,9 +358,9 @@ function calculate_columnwidth( self )
             end
 
             -- if stretch="no", we don't need to stretch/shrink anything
-            -- sum_real_widths == 0 happens when there are only * widths
+            -- count_stars == 0 if there are only fixed width columns
             -- given in the <Column width="..."/>  setting.
-            if self.autostretch ~= "max" and sum_real_widths ~= 0 then
+            if self.autostretch ~= "max" and count_stars == 0 then
                 self.tablewidth_target = sum_real_widths
             end
 
@@ -371,7 +371,7 @@ function calculate_columnwidth( self )
 
                 -- now we know the number of *-columns and the sum of the fix colums, so that
                 -- we can distribute the remaining space
-                local to_distribute = self.tablewidth_target - table.sum(self.column_distances,1,count_columns - 1)
+                local to_distribute = self.tablewidth_target - sum_real_widths - table.sum(self.column_distances,1,count_columns - 1)
                 i = 0
                 for _,column in ipairs(tr_contents) do
                     if publisher.elementname(column)=="Column" then

qa/tables/columntablerule/layout.xml
@@ -1,0 +1,118 @@
+<Layout xmlns="urn:speedata.de:2009/publisher/en"
+	xmlns:sd="urn:speedata:2009/publisher/functions/en">
+
+	<Record element="data">
+		<PlaceObject>
+			<Textblock>
+				<Paragraph>
+					<Value>Width, no stretch, column width provided.</Value>
+				</Paragraph>
+			</Textblock>
+		</PlaceObject>
+		<PlaceObject>
+			<Table width="150mm">
+				<Columns>
+					<Column width="7mm" align="left"/>
+					<Column width="1*" align="right"/>
+					<Column width="7mm" align="right"/>
+				</Columns>
+				<Tr valign="top">
+					<Td border-left="0.4pt" border-right="0.2pt"><Paragraph><Value>A</Value></Paragraph></Td>
+					<Td border-left="0.2pt" border-right="0.2pt"><Paragraph><Value>B</Value></Paragraph></Td>
+					<Td border-left="0.2pt" border-right="0.4pt"><Paragraph><Value>C</Value></Paragraph></Td>
+				</Tr>
+				<Tablerule/>
+			</Table>
+		</PlaceObject>
+		<NextRow/>
+		<PlaceObject>
+			<Textblock>
+				<Paragraph>
+					<Value>Width, no stretch, column width not provided.</Value>
+				</Paragraph>
+			</Textblock>
+		</PlaceObject>
+		<PlaceObject>
+			<Table width="150mm">
+				<Tr valign="top">
+					<Td border-left="0.4pt" border-right="0.2pt"><Paragraph><Value>A</Value></Paragraph></Td>
+					<Td border-left="0.2pt" border-right="0.2pt"><Paragraph><Value>B</Value></Paragraph></Td>
+					<Td border-left="0.2pt" border-right="0.4pt"><Paragraph><Value>C</Value></Paragraph></Td>
+				</Tr>
+				<Tablerule/>
+			</Table>
+		</PlaceObject>
+		<NextRow/>
+		<PlaceObject>
+			<Textblock>
+				<Paragraph>
+					<Value>No width, stretch, column width provided.</Value>
+				</Paragraph>
+			</Textblock>
+		</PlaceObject>
+		<PlaceObject>
+			<Table stretch="max">
+				<Columns>
+					<Column width="7mm" align="left"/>
+					<Column width="1*" align="right"/>
+					<Column width="7mm" align="right"/>
+				</Columns>
+				<Tr valign="top">
+					<Td border-left="0.4pt" border-right="0.2pt"><Paragraph><Value>A</Value></Paragraph></Td>
+					<Td border-left="0.2pt" border-right="0.2pt"><Paragraph><Value>B</Value></Paragraph></Td>
+					<Td border-left="0.2pt" border-right="0.4pt"><Paragraph><Value>C</Value></Paragraph></Td>
+				</Tr>
+				<Tablerule/>
+			</Table>
+		</PlaceObject>
+
+		<NextRow/>
+		<PlaceObject>
+			<Textblock>
+				<Paragraph>
+					<Value>No width, no stretch, column width provided.</Value>
+				</Paragraph>
+			</Textblock>
+		</PlaceObject>
+		<PlaceObject>
+			<Table>
+				<Columns>
+					<Column width="7mm" align="left"/>
+					<Column width="1*" align="right"/>
+					<Column width="7mm" align="right"/>
+				</Columns>
+				<Tr valign="top">
+					<Td border-left="0.4pt" border-right="0.2pt"><Paragraph><Value>A</Value></Paragraph></Td>
+					<Td border-left="0.2pt" border-right="0.2pt"><Paragraph><Value>B</Value></Paragraph></Td>
+					<Td border-left="0.2pt" border-right="0.4pt"><Paragraph><Value>C</Value></Paragraph></Td>
+				</Tr>
+				<Tablerule/>
+			</Table>
+		</PlaceObject>
+		<NextRow/>
+		<PlaceObject>
+			<Textblock>
+				<Paragraph>
+					<Value>No width, no stretch, column width provided. No *-columns.</Value>
+				</Paragraph>
+			</Textblock>
+		</PlaceObject>
+		<PlaceObject>
+			<Table>
+				<Columns>
+					<Column width="7mm" align="left"/>
+					<Column width="5cm" align="right"/>
+					<Column width="7mm" align="right"/>
+				</Columns>
+				<Tr valign="top">
+					<Td border-left="0.4pt" border-right="0.2pt"><Paragraph><Value>A</Value></Paragraph></Td>
+					<Td border-left="0.2pt" border-right="0.2pt"><Paragraph><Value>B</Value></Paragraph></Td>
+					<Td border-left="0.2pt" border-right="0.4pt"><Paragraph><Value>C</Value></Paragraph></Td>
+				</Tr>
+				<Tablerule/>
+			</Table>
+		</PlaceObject>
+
+	</Record>
+
+</Layout>

qa/tables/columntablerule/publisher.cfg
@@ -1,0 +1,1 @@
+dummy=true