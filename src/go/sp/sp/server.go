b52c095029212ecc803fdb2fa7188165b0d36dd4
Patrick Gundlach <gundlach@speedata.de>
2020-10-02 14:05:12 +0200

    server temporary directories start with any char

--- src/go/sp/sp/server.go
+++ src/go/sp/sp/server.go
@@ -439,8 +439,7 @@ func v0PublishHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Always start with non-0 to avoid problems out of scope of the publisher
-	tmpdir, err := ioutil.TempDir(serverTemp, "1")
+	tmpdir, err := ioutil.TempDir(serverTemp, "")
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		fmt.Fprintln(protocolFile, "Internal error 013:")
