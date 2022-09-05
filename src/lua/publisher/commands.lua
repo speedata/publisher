3ec03658635415b4eb2eaee7f1c93116a1ecd789
pr-apes <51094960+pr-apes@users.noreply.github.com>
2022-09-05 12:50:00 +0200

    new `displaymode` attribute for `<PDFOptions>` (2/3)

--- src/lua/publisher/commands.lua
+++ src/lua/publisher/commands.lua
@@ -2482,7 +2482,7 @@ function commands.pdfoptions( layoutxml, dataxml )
     local format       = publisher.read_attribute(layoutxml,dataxml,"format",    "string")
     local nc           = publisher.read_attribute(layoutxml,dataxml,"numcopies", "number")
     local printscaling = publisher.read_attribute(layoutxml,dataxml,"printscaling", "string")
-    local showbookmarks = publisher.read_attribute(layoutxml,dataxml,"showbookmarks", "boolean",true)
+    local displaymode = publisher.read_attribute(layoutxml,dataxml,"displaymode", "string")
     local picktray     = publisher.read_attribute(layoutxml,dataxml,"picktraybypdfsize", "boolean")
     local showhyperlinks = publisher.read_attribute(layoutxml,dataxml,"showhyperlinks", "boolean", false)
     local hyperlinksbordercolor = publisher.read_attribute(layoutxml,dataxml,"hyperlinksbordercolor", "string")
@@ -2518,8 +2518,17 @@ function commands.pdfoptions( layoutxml, dataxml )
     if nc then
         publisher.viewerpreferences.numcopies = nc
     end
-    if showbookmarks ~= nil then
-        publisher.viewerpreferences.showbookmarks = showbookmarks
+
+    if displaymode == "attachments" then
+        publisher.options.displaymode = "UseAttachments"
+    elseif displaymode == "bookmarks" then
+        publisher.options.displaymode = "UseOutlines"
+	elseif displaymode == "fullscreen" then
+        publisher.options.displaymode = "FullScreen"
+	elseif displaymode == "thumbnails" then
+        publisher.options.displaymode = "UseThumbs"
+	else
+        publisher.options.displaymode = "UseNone"
     end
 
     if printscaling then
